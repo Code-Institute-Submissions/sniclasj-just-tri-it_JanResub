23224751d5ec280acfb5bb07e07b6761837799fe
sniclasj <sniclasj@gmail.com>
2022-09-30 11:02:07 +0000

    listings/models.py

--- listings/models.py
+++ listings/models.py
@@ -12,10 +12,10 @@ class Category(models.Model):
 
 class Listing(models.Model):
     category = models.ForeignKey(
-        'Category', null=False, blank=False, on_delete=CASCADE)
+        'Category', null=False, blank=False, on_delete=models.CASCADE)
     name = models.CharField(max_length=254)
     condition = models.ForeignKey(
-        'Condition', null=False, blank=False, on_delete=CASCADE)
+        'Condition', null=False, blank=False, on_delete=models.CASCADE)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
