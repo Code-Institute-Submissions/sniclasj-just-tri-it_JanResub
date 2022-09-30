6b4f607df4777dfad99fcc25266c54e4d99ce8ab
sniclasj <sniclasj@gmail.com>
2022-09-30 14:22:10 +0000

    Add Profile model to profiles/models.py

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    athlete = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.athlete
