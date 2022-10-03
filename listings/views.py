edde306c0ba993a4e3edc210cb197854496efcb0
sniclasj <sniclasj@gmail.com>
2022-10-03 09:48:22 +0000

    Add content to listings/views.py

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Listing
 
 # Create your views here.
+
+
+def all_listings(request):
+
+    listings = Listing.objects.all
+
+    context = {
+        'listings': listings,
+    }
+
+    """A view to return the index page"""
+    return render(request, 'listings/listings.html', context)
