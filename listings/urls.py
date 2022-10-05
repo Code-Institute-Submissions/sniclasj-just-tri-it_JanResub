50e4ab4dd886d70d9ee07d96efc1b468353f7139
sniclasj <sniclasj@gmail.com>
2022-10-05 13:17:37 +0000

    Add url path for add_listing

--- listings/urls.py
+++ listings/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_listings, name='listings'),
-    path('<int:listing_id>', views.listing_info, name='listing_info'),
+    path('<int:listing_id>/', views.listing_info, name='listing_info'),
+    path('add/', views.add_listing, name='add_listing'),
 ]
