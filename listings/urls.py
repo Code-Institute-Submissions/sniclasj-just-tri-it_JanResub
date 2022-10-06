c08a67ea44e3414756f372989c7ae5d8d93578ab
sniclasj <sniclasj@gmail.com>
2022-10-06 08:40:48 +0000

    Add url path for delete_listing

--- listings/urls.py
+++ listings/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('<int:listing_id>/', views.listing_info, name='listing_info'),
     path('add/', views.add_listing, name='add_listing'),
     path('edit/<int:listing_id>/', views.edit_listing, name='edit_listing'),
+    path('delete/<int:listing_id>/', views.delete_listing, name='delete_listing'),
 ]
