4521aa374ef90592941d36fe2c1e840604f2a6d8
sniclasj <sniclasj@gmail.com>
2022-10-06 13:27:01 +0000

    Update url path to contain all_profiles and add profile_info path

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.profile, name='profile'),
+    path('', views.all_profiles, name='profiles'),
+    path('<int:profile_id>/', views.profile_info, name='profile_info'),
 ]
