10a282280d46e8b2047ba8be23735c1cd90cdd69
sniclasj <sniclasj@gmail.com>
2022-10-06 13:24:42 +0000

    Update url path from profile/ to profiles/

--- main/urls.py
+++ main/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('listings/', include('listings.urls')),
-    path('profile/', include('profiles.urls')),
+    path('profiles/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
