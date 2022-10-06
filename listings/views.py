3ec6f6c80ab0e0b31240c33489bdacb2b3f4ae5c
sniclasj <sniclasj@gmail.com>
2022-10-06 08:22:03 +0000

    Import redirect, reverse, messages and add conditional logic to add_listing form

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Listing, Category, Condition
 from .forms import ListingForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -30,8 +31,19 @@ def listing_info(request, listing_id):
 
 
 def add_listing(request):
-    """ Add a listing to the site """
-    form = ListingForm()
+    """ Add a listing to the store """
+    if request.method == 'POST':
+        form = ListingForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added listing!')
+            return redirect(reverse('add_listing'))
+        else:
+            messages.error(
+                request, 'Listing error. Please ensure the form is valid.')
+    else:
+        form = ListingForm()
+
     template = 'listings/add_listing.html'
     context = {
         'form': form,
