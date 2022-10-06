a0579e602633ff222f260ee3c362f7df6bf9d5a2
sniclasj <sniclasj@gmail.com>
2022-10-06 13:26:08 +0000

    Update profile view to be all_profiles and replicate all_listings format form listings model

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from .models import Profile
+# from .forms import Form
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
-def profile(request):
-    template = 'profiles/profile.html'
-    context = {}
+def all_profiles(request):
 
-    return render(request, template, context)
+    profiles = Profile.objects.all()
+
+    context = {
+        'profiles': profiles,
+    }
+
+    return render(request, 'profiles/profiles.html', context)
+
+
+def profile_info(request, profile_id):
+
+    profile = get_object_or_404(Profile, pk=profile_id)
+
+    context = {
+        'profile': profile,
+    }
+
+    """A view to return the profile_info page"""
+    return render(request, 'profiles/profile_info.html', context)
