c7978b6a485dd8f98d8631a316bb85b23535f5c0
sniclasj <sniclasj@gmail.com>
2022-10-08 09:21:09 +0000

    Remove redundant url path and add path for profile

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.all_profiles, name='profiles'),
-    path('<int:profile_id>/', views.profile_info, name='profile_info'),
+    path('', views.profile, name='profile'),
 ]
