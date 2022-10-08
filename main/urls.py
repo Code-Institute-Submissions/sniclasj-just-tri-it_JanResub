f29191c24679bd57c4aac7b21883426679e056b3
sniclasj <sniclasj@gmail.com>
2022-10-08 15:43:25 +0000

    Add basket url path to urlpatterns

--- main/urls.py
+++ main/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('listings/', include('listings.urls')),
     path('profiles/', include('profiles.urls')),
+    path('basket/', include('basket.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
