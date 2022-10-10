7043ab8746ebea061c7f32eb3a9cc39ad2054494
sniclasj <sniclasj@gmail.com>
2022-10-10 09:26:30 +0000

    Import ProfileForm and create edit_seller_status view

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Profile
-# from .forms import Form
+from .forms import ProfileForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
@@ -27,5 +27,36 @@ def profile_admin(request):
         'profiles': profiles,
     }
 
-    """A view to return the listings page"""
+    """A view to return the profile_admin page"""
     return render(request, 'profiles/profile_admin.html', context)
+
+
+@login_required
+def edit_seller_status(request, profile_id):
+    """ Edit a profile's seller status """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    profile = get_object_or_404(Profile, pk=profile_id)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, 'Successfully updated profile seller status!')
+            return redirect(reverse('profile_admin'))
+        else:
+            messages.error(
+                request, 'Update error. Please ensure the form is valid.')
+    else:
+        form = ProfileForm(instance=profile)
+        messages.info(request, f'You are editing {profile.athlete}')
+
+    template = 'profiles/edit_seller_status.html'
+    context = {
+        'form': form,
+        'profile': profile,
+    }
+
+    return render(request, template, context)
