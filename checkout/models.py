e7fd03c771b08cc4f6abbdba05aeb4ade0dec67d
sniclasj <sniclasj@gmail.com>
2022-10-10 13:13:43 +0000

    Correct line length and spacing to be pep8 compliant

--- checkout/models.py
+++ checkout/models.py
@@ -27,7 +27,6 @@ class Order(models.Model):
     grand_total = models.DecimalField(
         max_digits=10, decimal_places=2, null=False, default=0)
 
-
     def _generate_order_number(self):
         """
         Generate a random, unique order number using UUID
@@ -39,9 +38,12 @@ class Order(models.Model):
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(
+            Sum('lineitem_total'))['lineitem_total__sum']
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
-            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+            self.delivery_cost = (
+                (self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE) /
+                100)
         else:
             self.delivery_cost = 0
         self.grand_total = self.order_total + self.delivery_cost
