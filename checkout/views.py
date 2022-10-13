18929481fc32969a5c614c74b760cb07be88e132
sniclasj <sniclasj@gmail.com>
2022-10-13 09:45:19 +0000

    Add stripe and basket_contents import, define current_basket, total and stripe_total

--- checkout/views.py
+++ checkout/views.py
@@ -3,12 +3,20 @@ from django.contrib import messages
 
 from .forms import OrderForm
 
+from basket.contexts import basket_contents
+
+import stripe
+
 
 def checkout(request):
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, "There's nothing in your basket at the moment")
         return redirect(reverse('listings'))
+
+    current_basket = basket_contents(request)
+    total = current_basket['grand_total']
+    stripe_total = round(total * 100)
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
