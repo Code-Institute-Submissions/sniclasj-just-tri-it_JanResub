762f8386490411da409ffb5f9e8b62c53e167d03
sniclasj <sniclasj@gmail.com>
2022-10-17 08:02:39 +0000

    Import Profile model and add athlete FK to Order model

--- checkout/models.py
+++ checkout/models.py
@@ -4,12 +4,14 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 from django_countries.fields import CountryField
-
 from listings.models import Listing
+from profiles.models import Profile
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    athlete = models.ForeignKey(
+        Profile, on_delete=models.SET_NULL, blank=True, null=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
