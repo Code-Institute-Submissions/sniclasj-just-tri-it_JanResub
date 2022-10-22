5086b7090816a36377372e6f8b3cdca5972c70e0
sniclasj <sniclasj@gmail.com>
2022-10-22 14:38:33 +0000

    Add url paths for amend_basket and remove_from_basket

--- basket/urls.py
+++ basket/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.view_basket, name='view_basket'),
-    path('add/<int:item_id>', views.add_to_basket, name='add_to_basket')
+    path('add/<int:item_id>/', views.add_to_basket, name='add_to_basket'),
+    path('amend/<item_id>/', views.amend_basket, name='amend_basket'),
+    path('remove/<item_id>/', views.remove_from_basket, name='remove_from_basket'),
 ]
