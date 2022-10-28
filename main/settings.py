edcb8fb1516db3dd0579b06f46c55ab79871d9c4
sniclasj <sniclasj@gmail.com>
2022-10-28 12:50:23 +0000

    Correct typo in DATABASES

--- main/settings.py
+++ main/settings.py
@@ -115,7 +115,7 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_databse_url.parse(os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
