fa8866cf60839d6ce3f2215fe6fec1e2fb51339f
sniclasj <sniclasj@gmail.com>
2022-11-07 13:24:09 +0000

    Import Order model and define order_history view

--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,7 @@ from .forms import ProfileForm
 from .forms import SellerStatusForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from checkout.models import Order
 
 # Create your views here.
 
@@ -69,6 +70,23 @@ def edit_seller_status(request, profile_id):
     context = {
         'formb': formb,
         'profile': profile,
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
     }
 
     return render(request, template, context)
