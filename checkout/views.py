3de5864c010f1da686e4ace5b15f7504d0d1f715
sniclasj <sniclasj@gmail.com>
2022-11-07 13:58:58 +0000

    Add code to auto populate athlete profile details if authenticated

--- checkout/views.py
+++ checkout/views.py
@@ -106,7 +106,24 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        order_form = OrderForm()
+        if request.user.is_authenticated:
+            try:
+                profile = Profile.objects.get(athlete=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.athlete.get_full_name(),
+                    'email': profile.athlete.email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'county': profile.default_county,
+                })
+            except Profile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
