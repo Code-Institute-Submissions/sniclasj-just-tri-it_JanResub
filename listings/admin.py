e1bdf6dc5357ccbbd6b3d6ffcaf77ef136be4bfc
sniclasj <sniclasj@gmail.com>
2022-11-08 20:16:32 +0000

    Add ListingAdmin class to update view in admin

--- listings/admin.py
+++ listings/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import Listing, Category, Condition
 
 
-admin.site.register(Listing)
+@admin.register(Listing)
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'lister', 'category', 'condition', 'price')
+
+
 admin.site.register(Category)
 admin.site.register(Condition)
