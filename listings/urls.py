b8fe26c57a0b1b8bc384c28650590d21953a0522
sniclasj <sniclasj@gmail.com>
2022-11-10 10:22:48 +0000

    Add url path for add_condition

--- listings/urls.py
+++ listings/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('edit/<int:listing_id>/', views.edit_listing, name='edit_listing'),
     path('delete/<int:listing_id>/', views.delete_listing, name='delete_listing'),
     path('add_category/', views.add_category, name='add_category'),
+    path('add_condition/', views.add_condition, name='add_condition'),
 ]
