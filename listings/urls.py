bf596bc916b1045019778b932a5d8ae7c7d576f8
sniclasj <sniclasj@gmail.com>
2022-11-11 23:14:47 +0000

    Add url path for categories_conditions and edit_category

--- listings/urls.py
+++ listings/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     path('<int:listing_id>/', views.listing_info, name='listing_info'),
     path('add/', views.add_listing, name='add_listing'),
     path('edit/<int:listing_id>/', views.edit_listing, name='edit_listing'),
-    path('delete/<int:listing_id>/', views.delete_listing, name='delete_listing'),
+    path('delete/<int:listing_id>/', views.delete_listing, name='delete_listing'),  # noqa
     path('add_category/', views.add_category, name='add_category'),
     path('add_condition/', views.add_condition, name='add_condition'),
+    path('categories_conditions/', views.category_condition_admin, name='categories_conditons'),
+    path('edit_category/<int:category_id>', views.edit_category, name='edit_category'),
 ]
