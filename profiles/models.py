6b3f6df14dfdea66bc811e35db97310ceb7b0e3e
sniclasj <sniclasj@gmail.com>
2022-11-12 15:58:45 +0000

    Update syntax for pep8 compliance

--- profiles/models.py
+++ profiles/models.py
@@ -7,13 +7,17 @@ from django_countries.fields import CountryField
 
 class Profile(models.Model):
     athlete = models.OneToOneField(User, on_delete=models.CASCADE)
-    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_phone_number = models.CharField(
+        max_length=20, null=True, blank=True)
     default_country = CountryField(
         blank_label='Country *', null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
-    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
-    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_town_or_city = models.CharField(
+        max_length=40, null=True, blank=True)
+    default_street_address1 = models.CharField(
+        max_length=80, null=True, blank=True)
+    default_street_address2 = models.CharField(
+        max_length=80, null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
     is_seller = models.BooleanField(
         verbose_name='Is Seller?', null=False, default=False
