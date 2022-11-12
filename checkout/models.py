39efc6a97527f94f5d1a25723ee80b410bce6e21
sniclasj <sniclasj@gmail.com>
2022-11-12 15:35:38 +0000

    Update syntax for pep8 compliance

--- checkout/models.py
+++ checkout/models.py
@@ -11,7 +11,8 @@ from profiles.models import Profile
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     athlete = models.ForeignKey(
-        Profile, on_delete=models.SET_NULL, blank=True, null=True, related_name='orders')
+        Profile, on_delete=models.SET_NULL, blank=True, null=True,
+        related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
@@ -30,7 +31,8 @@ class Order(models.Model):
     grand_total = models.DecimalField(
         max_digits=10, decimal_places=2, null=False, default=0)
     original_basket = models.TextField(null=False, blank=False, default='')
-    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+    stripe_pid = models.CharField(
+        max_length=254, null=False, blank=False, default='')
 
     def _generate_order_number(self):
         """
@@ -69,7 +71,8 @@ class Order(models.Model):
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(
-        Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+        Order, null=False, blank=False,
+        on_delete=models.CASCADE, related_name='lineitems')
     listing = models.ForeignKey(
         Listing, null=False, blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
