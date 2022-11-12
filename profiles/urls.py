31196cc2a01d3eb2c3765e562b37123745beec8c
sniclasj <sniclasj@gmail.com>
2022-11-12 15:59:25 +0000

    Add #noqa to long url paths for pep8 compliance

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('admin/', views.profile_admin, name='profile_admin'),
-    path('admin/<int:profile_id>', views.edit_seller_status, name='edit_seller_status'),
-    path('order_history/<order_number>', views.order_history, name='order_history'),
+    path('admin/<int:profile_id>', views.edit_seller_status, name='edit_seller_status'),  # noqa
+    path('order_history/<order_number>', views.order_history, name='order_history'),  # noqa
 ]
