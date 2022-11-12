b6cf7b3ce2e6e2c73f7846366719f2ea5ecf6aed
sniclasj <sniclasj@gmail.com>
2022-11-12 15:53:54 +0000

    Update syntax to be pep8 compliant and remove redundant code

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -70,8 +70,10 @@ class StripeWH_Handler:
                 profile.default_country = shipping_details.address.country
                 profile.default_postcode = shipping_details.address.postal_code
                 profile.default_town_or_city = shipping_details.address.city
-                profile.default_street_address1 = shipping_details.address.line1
-                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_street_address1 = (
+                    shipping_details.address.line1)
+                profile.default_street_address2 = (
+                    shipping_details.address.line2)
                 profile.default_county = shipping_details.address.state
                 profile.save()
 
@@ -101,7 +103,7 @@ class StripeWH_Handler:
         if order_exists:
             self._send_email_confirmation(order)
             return HttpResponse(
-                content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
+                content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',  # noqa
                 status=200)
         else:
             order = None
@@ -128,15 +130,6 @@ class StripeWH_Handler:
                             quantity=item_data,
                         )
                         order_line_item.save()
-                    else:
-                        for size, quantity in item_data['items_by_size'].items():
-                            order_line_item = OrderLineItem(
-                                order=order,
-                                listing=listing,
-                                quantity=quantity,
-                                listing_size=size,
-                            )
-                            order_line_item.save()
             except Exception as e:
                 if order:
                     order.delete()
@@ -145,7 +138,7 @@ class StripeWH_Handler:
                     status=500)
         self._send_email_confirmation(order)
         return HttpResponse(
-            content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
+            content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',  # noqa
             status=200)
 
     def handle_payment_intent_payment_failed(self, event):
