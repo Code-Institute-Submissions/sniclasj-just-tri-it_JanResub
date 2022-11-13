13a346d3bdeef7d07e8f46ccac99d382f2aee7b9
sniclasj <sniclasj@gmail.com>
2022-11-13 05:00:05 +0000

    Remove redundant test

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,9 +1,3 @@
 from django.test import TestCase
-from .models import Profile
 
-
-class TestModels(TestCase):
-    def test_is_seller_defaults_to_false(self):
-        seller_status = Profile.is_seller.BooleanField(
-            'Test Default Seller Status')
-        self.assertFalse(seller_status.done)
+# Create your tests here.
