d81296340c379eeb6004be9920e3717dd47884f6
sniclasj <sniclasj@gmail.com>
2022-11-13 03:04:08 +0000

    Attempt untit test

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Profile
 
-# Create your tests here.
+
+class TestModels(TestCase):
+    def test_is_seller_defaults_to_false(self):
+        seller_status = Profile.is_seller.BooleanField(
+            'Test Default Seller Status')
+        self.assertFalse(seller_status.done)
